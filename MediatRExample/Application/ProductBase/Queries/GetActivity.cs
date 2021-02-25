2b2dc90e8a579b666c54d70780110f6933de4e68
ibrahimtaskin <taskin.ibrahim1@gmail.com>
2021-02-25 14:17:47 +0300

    MediatR Usage

---
+++ MediatRExample/Application/ProductBase/Queries/GetActivity.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using MediatRExample.Domain;
+using MediatRExample.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MediatRExample.Application.ProductBase.Queries
+{
+    public class GetActivity
+    {
+        public class Query : IRequest<Product>
+        {
+            public int Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Product>
+        {
+            private readonly MediatrContext _context;
+
+            public Handler(MediatrContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Product> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return await _context.Products.FindAsync(request.Id);
+            }
+        }
+    }
+}
---
+++ MediatRExample/Persistence/MediatrContext.cs
@@ -0,0 +1,28 @@
+﻿using MediatRExample.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MediatRExample.Persistence
+{
+    public class MediatrContext : DbContext
+    {
+        public MediatrContext(DbContextOptions<MediatrContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product { Id = 1, ProductName = "T-Shirt" },
+                new Product { Id = 2, ProductName = "Pantolon" },
+                new Product { Id = 3, ProductName = "Ceket" }
+                );
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
